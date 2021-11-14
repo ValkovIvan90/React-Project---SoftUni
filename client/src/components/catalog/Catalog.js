dabb0363d3259d44f3cc7d3f5f8f4bbea84e4b7d
Ivan Valkov <johnsson738@gmail.com>
2021-11-14 20:58:31 +0100

    catalog

---
+++ client/src/components/catalog/Catalog.js
@@ -0,0 +1,363 @@
+import './Catalog.css';
+
+
+import React from 'react'
+
+export default function Catalog() {
+    return (
+        <section className="catalog">
+            <article className="cards">
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://i.guim.co.uk/img/media/684c9d087dab923db1ce4057903f03293b07deac/205_132_1915_1150/master/1915.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=14a95b5026c1567b823629ba35c40aa0"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+                <article className="card">
+                    <div className="img">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcpc8ejmQTx8YEPGFdYUVnjc8p759Qz-R0Lg&usqp=CAU"
+                            alt=""
+                        />
+                    </div>
+                    <article className="info">
+                        <div className="card-info city">
+                            <h4>Vienna</h4>
+                        </div>
+                        <div className="card-info date">
+                            <p>Date</p>
+                        </div>
+                        <div className="card-info price">
+                            <p>Price</p>
+                        </div>
+                        <div className="card-info likes">
+                            <h4>Likes</h4>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="details-btn">Details</a>
+                        </div>
+                    </article>
+                </article>
+            </article>
+        </section>
+    )
+}
