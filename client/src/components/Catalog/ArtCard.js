dae789dfd280913c6bbb0bfcfa79922c13b8b82c
Ivan Valkov <johnsson738@gmail.com>
2021-11-16 06:32:29 +0100

    express

--- client/src/components/Catalog/ArtCard.js
+++ client/src/components/Catalog/ArtCard.js
@@ -7,19 +7,19 @@ export default function ArtCard({
         <article className="card">
             <div className="img">
                 <img
-                    src="https://i.guim.co.uk/img/media/684c9d087dab923db1ce4057903f03293b07deac/205_132_1915_1150/master/1915.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=14a95b5026c1567b823629ba35c40aa0"
-                    alt=""
+                    src={article.imageUrl}
+                    alt="art"
                 />
             </div>
             <article className="info">
                 <div className="card-info city">
-                    <h4>{article.title}</h4>
+                    <h4>{article.city}</h4>
                 </div>
                 <div className="card-info date">
-                    <p>{article.body}</p>
+                    <p>{article.year}</p>
                 </div>
                 <div className="card-info price">
-                    <p>{article.userId}</p>
+                    <p>{article.type}</p>
                 </div>
                 <div className="card-info likes">
                     <h4>Likes</h4>
