06d099cd260bafe0ab83b6e5cef78e79b9a8e780
Ivan Valkov <johnsson738@gmail.com>
2022-03-22 14:19:20 +0100

    de_ahwe

--- server/config/express.js
+++ server/config/express.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser');
 const auth = require('../middlewares/auth');
 const config = require('./config');
+const routes = require('./routes');
 
 module.exports = (app) => {
     app.use(express.urlencoded({
@@ -12,4 +13,5 @@ module.exports = (app) => {
     app.use(cookieParser());
     app.use(cors(config.CORS));
     app.use(auth());
+    app.use('/api', routes);
 }
--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ async function start() {
         app.use(express.static('client/build'));
 
         app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+            res.sendFile(path.resolve(__dirname, 'client', 'build'));
         });
     }
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
