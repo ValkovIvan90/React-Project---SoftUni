1616cd9438c5d3a3dc4d421c69f4fe01b9e2f577
Ivan Valkov <johnsson738@gmail.com>
2022-03-22 18:49:32 +0100

    route changes

--- server/index.js
+++ server/index.js
@@ -21,12 +21,12 @@ async function start() {
 
     app.use(await storage());
     routesConfig(app);
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static('client/build'));
 
-        app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-        });
-    }
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
 }
