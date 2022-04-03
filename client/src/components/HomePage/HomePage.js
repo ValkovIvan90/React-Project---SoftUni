356d8f9fd03b430573c1c8ec90ba9247eaf9ad18
Ivan Valkov <johnsson738@gmail.com>
2022-04-03 19:13:21 +0200

    response device chages

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -9,7 +9,7 @@ export default function HomePage() {
                 <img src={"images/homepage.jpg"} alt="" />
             </article>
             <article className="home-message">
-                <div className="home-text-box">
+                <div className="home-text-box" id='hm-txt-bx'>
                     <h1 className="home-message-title">I want to have ... everything</h1>
                     <p className="home-message-untertitle">The best free trade page !</p>
                 </div>
