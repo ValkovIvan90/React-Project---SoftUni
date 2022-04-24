f893900f1b9048db90e1fc63528cf7f901636fae
Ivan Valkov <johnsson738@gmail.com>
2022-04-24 22:10:28 +0200

    add fr_test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { render} from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('App', () => {
+
+  test('empty test', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+  });
 });
