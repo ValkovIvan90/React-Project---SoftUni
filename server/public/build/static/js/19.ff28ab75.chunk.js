ecbf1dac1e3aa5c255bc5cd31aa15e43887aa782
Ivan Valkov <johnsson738@gmail.com>
2022-04-25 18:58:18 +0200

    build folder

---
+++ server/public/build/static/js/19.ff28ab75.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[19],{320:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return o}));var c=n(0),s=n(9),u=n(2),i=n(1);function o(){return Object(c.useContext)(s.b).userData?Object(i.jsx)(u.b,{}):Object(i.jsx)(u.a,{to:"/login"})}}}]);
+//# sourceMappingURL=19.ff28ab75.chunk.js.map
---
+++ server/public/build/static/js/20.d738407c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[20],{321:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return i}));var c=n(0),r=n(9),s=n(2),u=n(10),b=n(1);function i(){var t=Object(c.useContext)(r.b).userData,e=Object(c.useContext)(u.b).currentArt;return t._id===e.owner?Object(b.jsx)(s.b,{}):Object(b.jsx)(s.a,{to:"/"})}}}]);
+//# sourceMappingURL=20.d738407c.chunk.js.map
